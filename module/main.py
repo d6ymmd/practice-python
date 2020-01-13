1d88a8c4d67bdd8a03f209900fc5e4cc7176ba56
bora shin <d6ymmd@gmail.com>
2020-01-13 14:43:37 +0900

    코딩도장 연습문제 풀이 및 직접 해보기

---
+++ module/main.py
@@ -0,0 +1,10 @@
+from person import Person
+import hello, calc
+
+# maria = Person('마리아', 20, '서울시 서초구 반포동')
+# maria.greeting()
+
+print('main.py __name__:', __name__)
+
+# calc.add(50,60)
+# calc.mul(50,60)
---
+++ module/main2.py
@@ -0,0 +1,12 @@
+# import clacpkg.operation
+# import clacpkg.geometry
+
+import clacpkg
+
+print(clacpkg.operation.add(10,20))
+print(clacpkg.operation.mul(10,20))
+
+print(clacpkg.geometry.triangle_area(30,40))
+print(clacpkg.geometry.rectangle_area(30,40))
+
+
